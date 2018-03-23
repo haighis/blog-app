b60e20ca937eed4acdd99388be69574d6b103e6b
John Haigh <haighis@gmail.com>
2018-03-22 22:42:22 -0400

    Implement CRUD
    - Add CRUD functionality to add a blog post and delete a blog post.
    - Update functionality is not implement as I had an issue getting this work.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,31 +1,66 @@
 import { Template } from 'meteor/templating';
 import { BlogPosts } from '../api/blogposts.js'; 
 import './body.html';
- 
+import { Session } from 'meteor/session'
+
+Session.set('editItemId', null);
+
+var updateBlogPost = function(){
+    var editItem = {
+      title: $("#editItemTitle").val()
+    }
+
+    BlogPosts.update(Session.get('editItemId'), {$set: editItem}, {validationContext: 'updateForm'}, function(error, result) {
+      if(!error){
+        Session.set('editItemId', null);
+      }
+    });
+}
+
+Template.addBlogPostForm.events({
+    'submit form': function(){
+        event.preventDefault();
+
+        const target = event.target;
+        const title = target.title.value;
+        const description = target.description.value;
+        const user = target.user.value;
+        
+        if(title !== '' &&
+          description !== '' && 
+          user !== '') {
+
+          // Insert Blog Post into the collection
+          BlogPosts.insert({
+            Title: title,
+            Description: description,
+            CreatedDate: new Date(),
+            User: user,
+          });
+       
+          // Clear form
+          target.title.value = '';
+          target.description.value = '';
+          target.user.value = '';
+        }
+    }
+});
+
+Template.body.events({
+  'click .delete': function() {
+    BlogPosts.remove(this._id);
+  },
+  'click .editItem': function(){
+    alert('todo implement')
+    Session.set('editItemId', this._id);
+  }
+});
+
 Template.body.helpers({
-  // Keep the dummy data around in case we ever want to revert back to this.  
-  // TODO move this to some library so it doesn't pollute.
-  // blogPosts: [
-  //   { 
-  //   	Title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-  //   	Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia, est vel ornare lacinia, sapien velit egestas ligula, nec congue erat risus eget dolor. Vivamus faucibus metus eget gravida fringilla.',
-  //   	CreatedDate: '23Apr2018',
-  //   	User: 'JohnHaigh'
- 	// },
-  //       { 
-  //   	Title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-  //   	Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia, est vel ornare lacinia, sapien velit egestas ligula, nec congue erat risus eget dolor. Vivamus faucibus metus eget gravida fringilla.',
-  //   	CreatedDate: '23Apr2018',
-  //   	User: 'JohnHaigh'
- 	// },
-  //   { 
-  //   	Title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-  //   	Description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse lacinia, est vel ornare lacinia, sapien velit egestas ligula, nec congue erat risus eget dolor. Vivamus faucibus metus eget gravida fringilla.',
-  //   	CreatedDate: '23Apr2018',
-  //   	User: 'JohnHaigh'
- 	// },
-  // ],
+  editing: function(){
+    return Session.equals('editItemId', this._id);
+  },
   blogPosts() {
-    return BlogPosts.find({});
+    return BlogPosts.find({}, { sort: { CreatedDate: -1 } });
   },
 });
